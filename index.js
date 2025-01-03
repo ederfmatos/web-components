253307c44ac92d9b41b8d8cd80ed04deedf1fdec
Eder Matos <ederfmatos@gmail.com>
2025-01-03 18:52:54 -0300

    addinput

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { MultiSelect } from './components/form-multi-select.js'
 import { Badge } from './components/ui-badge.js'
+import { Input } from './components/form-input.js'
 
 customElements.define('ui-badge', Badge);
+
+customElements.define('form-input', Input);
 customElements.define('form-multi-select', MultiSelect);
